0a30f055d1b41b8f4b10b900addf60e708feaaf8
Prachi Sahani <prachirsahani97@gmail.com>
2021-09-12 02:21:27 +0530

    added css and complete funcitonality

--- app.js
+++ app.js
@@ -2,15 +2,8 @@ var notesAvailable = [2000,500,100,20,10,5,1];
 var btnCheck = document.querySelector('#btn-check');
 var inputTotalBill = document.querySelector('#input-total-bill') ;
 var inputCashGiven = document.querySelector('#input-cash-given') 
-var noteCount = {
-    2000 : 0,
-    500 : 0,
-    100: 0,
-    20: 0,
-    10:0,
-    5: 0,
-    1: 0
-}
+var count = document.querySelectorAll('.count');
+var messageDiv = document.querySelector('#message')
 // inputTotalBill.addEventListener('change',enableCashGiven)
 
 btnCheck.addEventListener('click', validateTotalBillAndCashGiven);
@@ -31,44 +24,40 @@ function validateTotalBillAndCashGiven(){
 
     
     if( totalBill>0 && cashGiven>0 && cashGiven>=totalBill){
+        messageDiv.innerHTML="";
         calculateChange(totalBill,cashGiven);
         
     }
     else{
-        console.log('invalid input');
+        notesAvailable.forEach((note,index)=>{
+            count[index].innerHTML=""
+        })
+        if(totalBill<=0){
+            showMessage('Enter valid bill amount')
+        }
+        else if(cashGiven<totalBill){
+            showMessage('Ask for more cash!')
+        }
+        console.log('invalid');
     }
 
 }
 
+function showMessage(message){
+    messageDiv.innerHTML = message
+}
+
+
 function calculateChange(totalBill,cashGiven){
-    var noteCount = {
-        2000 : 0,
-        500 : 0,
-        100: 0,
-        20: 0,
-        10:0,
-        5: 0,
-        1: 0
-    }
-    if(cashGiven === totalBill){
-        return noteCount;
-    }
-    else{
         console.log('calculating');
         var amountToBeReturned = cashGiven- totalBill;
         var remainingAmount = amountToBeReturned;
-        notesAvailable.forEach((note)=>{
-            // console.log(note);
+        notesAvailable.forEach((note,index)=>{
             var numberOfNote = 0;
             if(remainingAmount >= note){
                 numberOfNote = Math.trunc(remainingAmount / note);
                 remainingAmount-= note*numberOfNote;
-                noteCount[note]=numberOfNote;
-                console.log(numberOfNote, remainingAmount);
-
             }
+            count[index].innerHTML=numberOfNote
         })
-        console.log(noteCount);
-    }
-    return noteCount;
 }
